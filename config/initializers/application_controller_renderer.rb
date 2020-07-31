26290284f5c31d2ca04e4a4756a79eb67c6ee581
角垣内渚里 <sumigaitonagisa@kakukakiuchinagisasatonoMacBook-Air.local>
2020-07-31 14:59:16 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eac9bce5d00f7a89398e53d48ff4d3e04b9d18f4f1fa62bb8374f0456bb1239561f155f237580760228f1785644e21d03ce73680fbdd328481dd6a2ea9e1dec3'
